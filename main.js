84ba0194fcbd14466ac3b40b85be4990b3cfa829
B1gbon <aldr4@bk.ru>
2020-06-17 05:16:06 +0300

    feature script.js type of variables

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 15000;
+let income = 'Фриланс';
+let addExpenses = 'коммуналка, такси, гитара';
+let deposit = 'true';
+let mission = '500000';
+let period = '11';
+let budgetDay = money / 30;
 
-
-alert('Hello World!');
-console.log('Hello World!');
+console.log( 'Тип данных money: ' + typeof money );
+console.log( 'Тип данных income: ' + typeof income );
+console.log( 'Тип данных deposit: ' + typeof deposit );
+console.log( 'длина addExpenses: ' + addExpenses.length );
+console.log( 'Период равен ' + period + ' месяцев' );
+console.log( 'Цель заработать ' + mission + ' рублей' );
+console.log( addExpenses.toLowerCase().split(' ') );
+console.log(budgetDay);
